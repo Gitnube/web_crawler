6fb908784039afbc54d55b3382a94f0f6c5769c2
Gitnube <dmitriyvbyf.comp@mail.ru>
2020-06-25 19:50:45 +0700

    Функция парсинга страницы переведена на использование функции для получения следующего токена согласно установленному фильтру

--- crawler.go
+++ crawler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 	"io"
@@ -13,7 +12,7 @@ import (
 )
 
 func main() {
-	cookieJar,_ := cookiejar.New(nil)
+	cookieJar, _ := cookiejar.New(nil)
 	client := &http.Client{
 		Jar: cookieJar,
 	}
@@ -21,7 +20,7 @@ func main() {
 	urlSend := "http://test.youplace.net/question/1"
 	res, _ = client.Get(urlSend)
 	var body []byte
-	for ;res.StatusCode == 200; {
+	for ; res.StatusCode == 200; {
 		form := PageProcessor(res.Body)
 		log.Println(form)
 		res.Body.Close()
@@ -32,68 +31,79 @@ func main() {
 	log.Println(res.Request.URL)
 }
 
-func PageProcessor(body io.Reader) (form url.Values) {
-	page := html.NewTokenizer(body)
-
-	var start *html.Token
-	var text string
-	form = make(url.Values)
-	var selectName string
+func NextToken(page *html.Tokenizer) (tag html.Token) {
+	neededTokenTypes := map[atom.Atom]bool{
+		atom.Form:   true,
+		atom.Select: true,
+		atom.Option: true,
+		atom.Input:  true,
+		atom.Title:  true,
+		atom.A: true,
+	}
 
 	for {
 		_ = page.Next()
 		token := page.Token()
 		if token.Type == html.ErrorToken {
 			if page.Err() != io.EOF {
-				fmt.Printf("Ошибка\n")
+				log.Println("Ошибка парсинга тела http-ответа")
 			}
+			return token
+		}
+
+		if neededTokenTypes[token.DataAtom] && token.Type == html.StartTagToken {
+			return token
+		}
+	}
+}
+
+func PageProcessor(body io.Reader) (form url.Values) {
+	page := html.NewTokenizer(body)
+
+	form = make(url.Values)
+	var selectName string
+
+	for {
+		token := NextToken(page)
+		if token.Type == html.ErrorToken {
 			break
 		}
-
-		if start != nil && token.Type == html.TextToken {
+		/*if token.Type == html.TextToken {
 			text = fmt.Sprintf("%s%s", text, token.Data)
-		}
-
-		if token.DataAtom == atom.Input {
-			if token.Type == html.StartTagToken {
-				name, inpType, value := InputProcessor(token)
-				if inpType == "text" {
-					form.Add(name, "test")
-				} else if inpType == "radio" {
-					radioValue, exists := form[name]
-					if !exists || len(radioValue[0]) < len(value) {
-						form.Set(name, value)
-					}
+		}*/
+		switch token.DataAtom {
+		case atom.Input:
+			name, inpType, value := InputProcessor(token)
+			if inpType == "text" {
+				form.Add(name, "test")
+			} else if inpType == "radio" {
+				radioValue, exists := form[name]
+				if !exists || len(radioValue[0]) < len(value) {
+					form.Set(name, value)
 				}
 			}
-		} else if token.DataAtom == atom.Select && token.Type == html.StartTagToken {
+		case atom.Select:
 			selectName, _, _ = InputProcessor(token)
-		}
-		if token.DataAtom == atom.Option {
-			switch token.Type {
-			case html.StartTagToken:
-				if len(token.Attr) > 0 {
-					start = &token
-				}
-			case html.EndTagToken:
-				if start == nil {
-					continue
-				}
-				_, _, value := InputProcessor(*start)
-				selectValue, exists := form[selectName]
-				if !exists || len(selectValue[0]) < len(value) {
-					form.Set(selectName, value)
-				}
-
-				start = nil
-				text = ""
+		case atom.Option:
+			_, _, value := InputProcessor(token)
+			selectValue, exists := form[selectName]
+			if !exists || len(selectValue[0]) < len(value) {
+				form.Set(selectName, value)
 			}
+		case atom.Title:
+			_ = page.Next()
+			textToken := page.Token()
+			if textToken.Type == html.TextToken {
+				log.Println(textToken.Data)
+			}
+		case atom.A:
+			
 		}
 	}
 	return form
 }
 
-func InputProcessor(tag html.Token) (name, inpType, value string) {
+func InputProcessor(tag html.Token) (name, inpType, value string) {//пусть возвращает карту аттрибутов (ключ-значение)
 	for i := range tag.Attr {
 		if tag.Attr[i].Key == "type" {
 			inpType = strings.TrimSpace(tag.Attr[i].Val)
